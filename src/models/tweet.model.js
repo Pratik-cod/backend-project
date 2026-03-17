2d42da847f290ee8612a81f85f232734c295a12e
Pratik-cod <ranepratik13@gmail.com>
2026-03-17 23:07:47 +0530

    Add tweet model with schema definition for owner and content

---
+++ src/models/tweet.model.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const tweetSchema = new mongoose.Schema({
+owner:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"user"
+},
+content:{
+    type:String,
+    required:true
+}
+
+},{timestamps:true})
+
+export const tweet = mongoose.model("tweet",tweetSchema)

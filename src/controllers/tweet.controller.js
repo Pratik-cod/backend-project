d86b3ad6e94acf2561d52442d07b8f9d9818e15c
Pratik-cod <ranepratik13@gmail.com>
2026-03-31 14:52:12 +0530

    Add createTweet function for tweet creation with validation and error handling

---
+++ src/controllers/tweet.controller.js
@@ -0,0 +1,29 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/apiError.js";
+import { ApiResponce } from "../utils/apiResponce.js";
+import { tweet } from "../models/tweet.model.js";
+
+const createTweet = asyncHandler(async(req,res) => {
+    const {content} = req.body
+    const UserId = req.user._id
+
+    if(!content){
+        throw new ApiError(400,"tweet is required")
+    }
+    if(!UserId){
+        throw new ApiError(400,"unautiorized")
+    }
+
+    const creationTweet = await tweet.create({
+        owner:UserId,
+        content:content
+    })
+    if(!creationTweet){
+        throw new ApiError(400,"creation of tweet is failed")
+    }
+    res.status(200).json(new ApiResponce(200,createTweet,"Tweet creation successfully"))
+})
+
+export {
+    createTweet
+}

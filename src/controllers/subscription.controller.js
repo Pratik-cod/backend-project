08a2ae1156f40ed0712c07f5ce22ad033198c5a9
Pratik-cod <ranepratik13@gmail.com>
2026-04-01 10:52:10 +0530

    Refactor toggleSubscription function for improved error handling and clarity

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -2,42 +2,32 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/apiError.js";
 import { ApiResponce } from "../utils/apiResponce.js";
 import { subscription } from "../models/subsciption.model.js";
+import mongoose from "mongoose";
+import { user as User } from "../models/User.model.js";
 
-const toggleSubscription = asyncHandler(async(req,res) => {
-    const {channelId} = req.params
-    const UserId  = req.user._id
+const toggleSubscription = asyncHandler(async (req, res) => {
+  const { channelId } = req.params;
+  const subscriberId = req.user._id;
 
-    if(!channelId){
-        throw new ApiError(400,"channelId is required")
-    }
-    if(!UserId){
-        throw new ApiError(400,"unauthorized")
-    }
+  if (!channelId) throw new ApiError(400, "channelId is required");
+  if (!subscriberId) throw new ApiError(400, "subscriber id not found");
 
-    const findChannel = await subscription.findById(channelId)
+  const findChannel = await User.findById(channelId);
+  if (!findChannel) throw new ApiError(404, "Channel user not found");
 
-    if(!findChannel){
-        throw new ApiError(400,"not found  channelId")
-    }
-    const existingsubsciber = await subscription.findOne({
-        UserId,
-        channelId
-    })
-    if(existingsubsciber){
-        await subscription.deleteOne({
-            UserId,
-            channelId
-        })
-    }else{
-        await subscription.create({
-            UserId,
-            channelId
-        })
-    }
-res.status(200).json(new ApiResponce(200,existingsubsciber,"user are subscribe"))
-})
+  const existing = await subscription.findOne({
+    subscriber: subscriberId,
+    channel: channelId
+  });
 
-
+  if (existing) {
+    await subscription.deleteOne({ subscriber: subscriberId, channel: channelId });
+    return res.status(200).json(new ApiResponce(200, null, "Unsubscribed successfully"));
+  } else {
+    const newSub = await subscription.create({ subscriber: subscriberId, channel: channelId });
+    return res.status(200).json(new ApiResponce(200, newSub, "Subscribed successfully"));
+  }
+});
 
 export {
     toggleSubscription,getSubscribedChannels,getUserChannelsubscibers

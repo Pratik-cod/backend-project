1ed3d2d4f914402477a2b8861198604a6922b802
Pratik-cod <ranepratik13@gmail.com>
2026-04-04 14:09:24 +0530

    Fix reference strings in subscription model for subscriber and channel fields

--- src/models/subsciption.model.js
+++ src/models/subsciption.model.js
@@ -4,11 +4,11 @@ const subscriptionSchema = new Schema({
 
     subscriber:{
         type:Schema.Types.ObjectId,
-        ref:user
+        ref:"user"
     },
     channel:{
         type:Schema.Types.ObjectId,
-        ref:user
+        ref:"user"
     }
 },{timestamps:true})
 

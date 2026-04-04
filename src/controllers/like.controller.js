1b7bc7023ef44047ea7c5bf1377af1fa557fedd9
Pratik-cod <ranepratik13@gmail.com>
2026-04-04 13:41:52 +0530

    Refactor like controller for improved code clarity and consistency

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,60 +1,50 @@
-import { asyncHandler } from "../utils/asyncHandler";
-import { ApiError } from "../utils/apiError";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/apiError.js";
 import { like } from "../models/like.model.js";
 import { user } from "../models/User.model.js";
 import { video } from "../models/video.model.js";
 import {Comment} from "../models/comments.model.js"
 import {tweet} from '../models/tweet.model.js'
+import { ApiResponce } from "../utils/apiResponce.js";
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
-    const { videoId, userId } = req.params;
+  const { videoId } = req.params;
+  const userId = req.user._id;
 
-    // validate input
-    if (!videoId) {
-        throw new ApiError(400, "videoId is required");
-    }
+  if (!videoId) throw new ApiError(400, "videoId is required");
+  if (!userId) throw new ApiError(400, "userId is required");
 
-    if (!userId) {
-        throw new ApiError(400, "userId is required");
-    }
+  // check user exists
+  const findUser = await user.findById(userId);
+  if (!findUser) throw new ApiError(404, "User not found");
 
-    // check user exists
-    const findUser = await user.findById(userId);
-    if (!findUser) {
-        throw new ApiError(404, "User not found");
-    }
+  // check video exists
+  const findVideo = await video.findById(videoId);
+  if (!findVideo) throw new ApiError(404, "Video not found");
 
-    // check video exists
-    const findVideo = await video.findById(videoId);
-    if (!findVideo) {
-        throw new ApiError(404, "Video not found");
-    }
+  // check like exists
+  const existingLike = await like.findOne({ video: videoId, likeby: userId });
 
-    // check like exists
-    const existingLike = await like.findOne({
-        videoId,
-        userId
-    });
+  if (existingLike) {
+    // unlike
+    await like.deleteOne({ video: videoId, likeby: userId });
+     return res
+      .status(200)
+      .json(new ApiResponce(200, "Video unliked", null, true));
 
-    if (existingLike) {
-        // unlike
-        await like.deleteOne({ userId, videoId });
+  }
 
-        return res.status(200).json({
-            message: "Video unliked"
-        });
-    } else {
-        // like
-        await like.create({ userId, videoId });
+  // like
+  const newLike = await like.create({ video: videoId, likeby: userId });
+    return res
+    .status(200)
+    .json(new ApiResponce(200, "Video liked successfully", newLike, true));
 
-        return res.status(200).json({
-            message: "Video liked"
-        });
-    }
 });
 
 const commentLikeToggle = asyncHandler(async (req,res) => {
-    const {commentId,userId} = req.params
+    const {commentId} = req.params
+    const userId = req.user._id
 
     if(!commentId){
         throw new ApiError(401," commentId is required")
@@ -80,48 +70,17 @@ const commentLikeToggle = asyncHandler(async (req,res) => {
     })
     if(existingcommentLike){
         await like.deleteOne({userId,commentId})
+             return res
+      .status(200)
+      .json(new ApiResponce(200, "Comment unliked", null, true));
     }
-    else{
-        await like.create({
-            userId,commentId
-        })
-    }
+
+ const newLike = await like.create({ userId, commentId });
+    return res
+    .status(200)
+    .json(new ApiResponce(200, "Comment liked successfully", newLike, true));
+
 })
 
-const TweetLiketoggle = asyncHandler(async (req,res) => {
-    const {tweetId,userId} = req.params
-
-    if(!tweetId){
-        throw new ApiError(404,"tweetId is required")
-    }
-    if(!userId){
-        throw new ApiError(401,"unauthorized user")
-    }
-    const findtweet = await tweet.findById(tweetId)
-    if(!findtweet){
-        throw new ApiError(404,"tweet is not found")
-    }
-    const findUser = await user.findById(userId)
-    if(!findUser){
-        throw new ApiError(404,"user is not found")
-    }
-
-    const existingTweetLike = await tweet.findOne({
-        tweetId,
-        userId
-    })
-    if(existingTweetLike){
-   await tweet.deleteOne({
-      userId,
-      tweetId
-   })
-    }else{
-        await tweet.create({
-            tweetId,
-            userId
-        })
-    }
-})
-
-export { toggleVideoLike,commentLikeToggle,TweetLiketoggle };
+export { toggleVideoLike,commentLikeToggle,TweetLiketoggle,getLikeVideo };
 

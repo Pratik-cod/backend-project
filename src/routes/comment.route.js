1b62a42400d18802ca57134d3f98d0521d745eef
Pratik-cod <ranepratik13@gmail.com>
2026-04-06 10:15:21 +0530

    adding the coomment route

---
+++ src/routes/comment.route.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import {
+    addComment,updateComments,deleteComments,getVideoComments
+} from "../controllers/comment.controller.js"
+import {verifyJWT} from "../middlewares/auth.middleware.js"
+
+const router = Router();
+
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+
+ router.route("/:videoId").get(getVideoComments).post(addComment);
+router.route("/c/:commentId").delete(deleteComments).patch(updateComments);
+
+export default router
